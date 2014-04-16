5d4c93ccb39a484786ca74d4f7f9d83f36aaaab9
magnusknutas <magnus@thefarm.se>
2014-04-16 15:22:15 +0200

    Very ugly solution

--- socket_transfer/namespace.py
+++ socket_transfer/namespace.py
@@ -61,7 +61,7 @@ class BaseEsNamespace(BaseNamespace):
             self.disconnect(silent=True)
             logger.debug("Disconnect: %s", str(packet))
             ret = self.call_method_with_acl('recv_disconnect', packet)
-            OnlineUsers.objects.delete(user=get_user(self.environ))
+            OnlineUsers.objects.get_or_create(user=get_user(self.environ)).delete()
         try:
             connection.close()
         except:
