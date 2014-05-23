e01bf5732b089f26b30c7f2ab849a998682ac3d7
magnusknutas <magnus@thefarm.se>
2014-05-23 09:14:11 +0200

    Trying atomic transaction

--- socket_transfer/namespace.py
+++ socket_transfer/namespace.py
@@ -1,5 +1,5 @@
 import logging
-from django.db import connection
+from django.db import connection, transaction
 from socketio.namespace import BaseNamespace
 from socket_transfer.models import OnlineUsers
 from socket_transfer.socket_server import get_user
@@ -26,50 +26,47 @@ class BaseEsNamespace(BaseNamespace):
         * on_*()
         """
         #connection.open()
-        packet_type = packet['type']
-        if packet_type == 'event':
-            logger.info("Event: %s", str(packet))
-            ret = self.process_event(packet)
-        elif packet_type == 'message':
-            logger.debug("Message: %s", packet['data'])
-            ret = self.call_method_with_acl('recv_message', packet,
-                                             packet['data'])
-        elif packet_type == 'json':
-            logger.debug("json: %s", packet['data'])
-            ret = self.call_method_with_acl('recv_json', packet,
-                                             packet['data'])
-        elif packet_type == 'connect':
-            logger.debug("Connect: %s", str(packet))
-            ret = self.call_method_with_acl('recv_connect', packet)
-            try:
-                OnlineUsers.objects.get_or_create(user_id=get_user(self.environ).pk)
-            except:
-                pass
-        elif packet_type == 'error':
-            logger.debug("Error: %s", str(packet))
-            ret = self.call_method_with_acl('recv_error', packet)
-        elif packet_type == 'ack':
-            logger.debug("Ack: %s", str(packet['ackId']))
-            callback = self.socket._pop_ack_callback(packet['ackId'])
-            if not callback:
-                logger.error("No such callback for ackId %s", packet['ackId'])
+        with transaction.atomic():
+            packet_type = packet['type']
+            if packet_type == 'event':
+                logger.info("Event: %s", str(packet))
+                ret = self.process_event(packet)
+            elif packet_type == 'message':
+                logger.debug("Message: %s", packet['data'])
+                ret = self.call_method_with_acl('recv_message', packet,
+                                                 packet['data'])
+            elif packet_type == 'json':
+                logger.debug("json: %s", packet['data'])
+                ret = self.call_method_with_acl('recv_json', packet,
+                                                 packet['data'])
+            elif packet_type == 'connect':
+                logger.debug("Connect: %s", str(packet))
+                ret = self.call_method_with_acl('recv_connect', packet)
+                try:
+                    OnlineUsers.objects.get_or_create(user_id=get_user(self.environ).pk)
+                except:
+                    pass
+            elif packet_type == 'error':
+                logger.debug("Error: %s", str(packet))
+                ret = self.call_method_with_acl('recv_error', packet)
+            elif packet_type == 'ack':
+                logger.debug("Ack: %s", str(packet['ackId']))
+                callback = self.socket._pop_ack_callback(packet['ackId'])
+                if not callback:
+                    logger.error("No such callback for ackId %s", packet['ackId'])
 
-            try:
-                ret = callback(*(packet['args']))
-            except TypeError, e:
-                logger.error("Call to callback function failed %s", str(e.message))
-        elif packet_type != 'disconnect':
-            logger.warning("Unprocessed packet %s", str(packet))
-        elif packet_type == 'disconnect':
-            self.disconnect(silent=True)
-            logger.debug("Disconnect: %s", str(packet))
-            ret = self.call_method_with_acl('recv_disconnect', packet)
-            try:
-                OnlineUsers.objects.get_or_create(user=get_user(self.environ))[0].delete()
-            except:
-                pass
-        try:
-            connection.close()
-        except:
-            logger.error("CANT CLOSE DATABASE")
+                try:
+                    ret = callback(*(packet['args']))
+                except TypeError, e:
+                    logger.error("Call to callback function failed %s", str(e.message))
+            elif packet_type != 'disconnect':
+                logger.warning("Unprocessed packet %s", str(packet))
+            elif packet_type == 'disconnect':
+                self.disconnect(silent=True)
+                logger.debug("Disconnect: %s", str(packet))
+                ret = self.call_method_with_acl('recv_disconnect', packet)
+                try:
+                    OnlineUsers.objects.get_or_create(user=get_user(self.environ))[0].delete()
+                except:
+                    pass
         return ret
