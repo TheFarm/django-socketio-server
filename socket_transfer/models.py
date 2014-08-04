b39b4674fe944d486c1c4103ebc5e16016662b0a
Tobias Alfredsson <tobias@thefarm.se>
2014-08-04 10:16:39 +0200

    addded online users __unicode__

--- socket_transfer/models.py
+++ socket_transfer/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class OnlineUsers(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user.username
